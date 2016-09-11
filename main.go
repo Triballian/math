ee23a6451cb4a6c08617edb8732b51b2a51e1d8f
Triballian <candiapps@gmail.com>
2016-09-11 08:15:59 -0500

    code cleanup

--- main.go
+++ main.go
@@ -16,32 +16,21 @@ import (
 	"unicode"
 )
 
-//numofterms := 2
-//term := []string{2x**2, 3y**2}
-//func getterm(s string) string {
-//
-//}
-//for i := 0; i < range term; i++{
-//}
-//for i := 0; i < numofterms; i++ {
-//fmt.Println("%s", term1)
-//}
+
 func main() {
 	//infinite loop to continue evaluating the input expressions
-	// var in string
-	// var fmtdequation string = ""
+
 	for {
 
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("\nType the equation to be identified as either a function or not a function")
 		fmt.Print("\n>>")
-		//line := in.ReadString(" ")
+
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		//element := strings.Split(text, " ")
-		//var t rune
+
 		var ftext string
 
 		ftext = strings.Map(func(t rune) rune {
@@ -59,32 +48,8 @@ func main() {
 		}
 		fmt.Printf("left side of equation:%s, Right side of equtioin:%s\n", expression[0], expression[1])
 
-		//remove spaces -1 teslls Replace that there is no limit to the number of replacements
-		//fmtdequation := strings.Replace(equation, " ", "", -1)
 
-		//fmt.Println(fmtdequation)
-		//fmt.Println(equation)
-		// sepreate by =
-		// seperate by arithic operator
-
-		//for i := 0; i < numofterms; i++ {
-		//	fmt.Println("%s", term1)
 	}
 }
 
-// fmt.Println(fmtdequation)
-//}
 
-//func simpleReadLine(r io.Reader) (string, error) {
-//	rl := bufio.NewScanner(r)
-//	rl.Scan()
-//	if err := rl.Err(); err != nil {
-//		return "", err
-//	}
-//	return rl.Text(), nil
-//}
-//
-//func main() {
-//	r := strings.NewReader(fmt.scanln())
-//	fmt.Println(simpleReadLine(r))
-//}
