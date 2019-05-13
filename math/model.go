69aa66af582bb9a1d42fdd8fd88b923696455f3b
Triballian <candiapps@gmail.com>
2019-05-13 12:03:37 -0500

    some order of operations

--- math/model.go
+++ math/model.go
@@ -8,14 +8,15 @@ package math
 type Term interface {
 	// if the var is alone then the nubmber is zero
 	// every term has a coefficietn even if that coeficient is one
+	SetIsParenthesesed(bool) Term
+	SetIsSquareRooted(bool) Term // bool
+	SetExponent(int) Term
 	SetNumericCoefficient(int) Term
 	SetVariableCoefficient(rune) Term // x or y or empty '' rune, convert any to lowercase
 	// each side of an equation is an expression
 	// types of coeficeint positive, negative, fractional
-	SetIsSquareRooted(bool) // bool
 
-	SetExponent(int) Term
-	GetTerm() Term
+	GetTerm() TermP
 	// is it a paranthesized expression, is it a square rooted expression
 	// is it a paranthesized term, is it a square rooted term
 	// use slices to hold expressions so that size can be easily determined
@@ -35,28 +36,29 @@ var tmaninstance *TManufacturingDirector
 
 // term product
 
-func GetManInstance() *TManufacturingDirector {
-	if taninstance == nil {
-		maninstance = new(TManufacturingDirector)
+func GetTManInstance() *TManufacturingDirector {
+	if tmaninstance == nil {
+		tmaninstance = new(TManufacturingDirector)
 	}
 	return tmaninstance
 }
 
 // TConstruct construct method
-func (f *TManufacturingDirector) TConstruct(termcount int, abssets int, operators int) {
-	f.builder.SetNumericCoefficient(NumericCoefficient).SetVariableCoefficient(VariableCoefficient).IsSquareRooted(bool).SetExponent(Exponent)
+func (f *TManufacturingDirector) TConstruct(numericCoefficient int, variableCoefficient rune, isSquareRooted bool, exponent int) {
+	f.builder.SetNumericCoefficient(numericCoefficient).SetVariableCoefficient(variableCoefficient).SetIsSquareRooted(isSquareRooted).SetExponent(exponent)
 }
 
 //EManufacturingDirector Expression Manufacturing Director
-func (f *EManufacturingDirector) SetTBuilder(t Term) {
-	t.builder = b
+func (f *TManufacturingDirector) SetTBuilder(t Term) {
+	f.builder = t
 }
 
 type TermP struct {
+	IsParenthesesed     bool
+	SquareRooted        bool
+	Exponent            int
 	NumericCoefficient  int
 	VariableCoefficient rune
-	IsSquareRooted      bool
-	Exponent            int
 }
 
 // type TermBuilder struct {
@@ -64,33 +66,161 @@ type TermP struct {
 // 	mt TermProduct
 // }
 
-type ConstantTern struct {
+type ConstantTerm struct {
 	v TermP
 }
 
-func (t *TermP) SetNumericCoefficient(n int) *TermP {
-	t.Number = n
-	return t
+//even thought the types are created virtually identically we can identify instance for its specific type symplifying or even dictating the operations that are applied to it.
+func (c *ConstantTerm) SetIsParenthesesed(s bool) Term {
+	c.v.IsParenthesesed = s
+	return c
 }
-func (t *TermP) SetVariableCoefficient(v rune) *TermP {
-	t.VariableCoefficient = v
-	return t
+func (c *ConstantTerm) SetIsSquareRooted(s bool) Term {
+	c.v.IsSquareRooted = s
+	return c
 }
-func (t *TermP) SetExponent(n int) *TermP {
-	t.Exponent = n
-	return t
+func (c *ConstantTerm) SetExponent(e int) Term {
+	c.v.Exponent = e
+	return c
 }
-func (t *TermP) Setvars(thisvar string, varval bool) *TermP {
-	if thisvar == "x" || thisvar == "X" {
-		t.Xvar = varval
-	}
-	if thisvar == "y" || thisvar == "Y" {
-		t.Yvar = varval
-	}
+func (c *ConstantTerm) SetNumericCoefficient(n int) Term {
+	c.v.NumericCoefficient = n
+	return c
+}
+func (c *ConstantTerm) SetVariableCoefficient(v rune) Term {
+	c.v.VariableCoefficient = v
+	return c
+}
 
-	return t
+// GetTerm get term function
+// func (t *Term) Setvars(thisvar string, varval bool) *TermP {
+// 	if thisvar == "x" || thisvar == "X" {
+// 		t.Xvar = varval
+// 	}
+// 	if thisvar == "y" || thisvar == "Y" {
+// 		t.Yvar = varval
+// 	}
+
+// 	return t
+// }
+
+func (c *ConstantTerm) GetTerm() TermP {
+	return c.v
 }
 
-func (t *TermP) GetTerm(x bool) *TermP {
-	return t
+type VarTerm struct {
+	v TermP
+}
+
+func (c *ConstantTerm) SetIsParenthesesed(s bool) Term {
+	c.v.IsParenthesesed = s
+	return c
+}
+func (c *VarTerm) SetIsSquareRooted(s bool) Term {
+	c.v.SquareRooted = s
+	return c
+}
+func (c *VarTerm) SetExponent(e int) Term {
+	c.v.Exponent = e
+	return c
+}
+func (c *VarTerm) SetNumericCoefficient(n int) Term {
+	c.v.NumericCoefficient = n
+	return c
+}
+func (c *VarTerm) SetVariableCoefficient(v rune) Term {
+	c.v.VariableCoefficient = v
+	return c
+}
+
+func (c *VarTerm) GetTerm() TermP {
+	return c.v
+}
+
+type VarCoeefficientTerm struct {
+	v TermP
+}
+
+func (c *ConstantTerm) SetIsParenthesesed(s bool) Term {
+	c.v.IsParenthesesed = s
+	return c
+}
+func (c *VarCoeefficientTerm) SetIsSquareRooted(s bool) Term {
+	c.v.SquareRooted = s
+	return c
+}
+
+func (c *VarCoeefficientTerm) SetExponent(e int) Term {
+	c.v.Exponent = e
+	return c
+}
+func (c *VarCoeefficientTerm) SetNumericCoefficient(n int) Term {
+	c.v.NumericCoefficient = n
+	return c
+}
+func (c *VarCoeefficientTerm) SetVariableCoefficient(v rune) Term {
+	c.v.VariableCoefficient = v
+	return c
+}
+
+func (c *VarCoeefficientTerm) GetTerm() TermP {
+	return c.v
+}
+
+type SquareRootedTerm struct {
+	v TermP
+}
+
+func (c *SquareRootedTerm) SetIsParenthesesed(s bool) Term {
+	c.v.IsParenthesesed = s
+	return c
+}
+func (c *SquareRootedTerm) SetIsSquareRooted(s bool) Term {
+	c.v.SquareRooted = s
+	return c
+}
+func (c *SquareRootedTerm) SetExponent(e int) Term {
+	c.v.Exponent = e
+	return c
+}
+func (c *SquareRootedTerm) SetNumericCoefficient(n int) Term {
+	c.v.NumericCoefficient = n
+	return c
+}
+func (c *SquareRootedTerm) SetVariableCoefficient(v rune) Term {
+	c.v.VariableCoefficient = v
+	return c
+}
+
+func (c *SquareRootedTerm) GetTerm() TermP {
+	return c.v
+}
+
+type ExponentedTerm struct {
+	v TermP
+}
+
+func (c *ExponentedTerm) SetIsParenthesesed(s bool) Term {
+	c.v.IsParenthesesed = s
+	return c
+}
+func (c *ExponentedTerm) SetExponent(e int) Term {
+	c.v.Exponent = e
+	return c
+}
+func (c *ExponentedTerm) SetIsSquareRooted(s bool) Term {
+	c.v.SquareRooted = s
+	return c
+}
+func (c *ExponentedTerm) SetNumericCoefficient(n int) Term {
+	c.v.NumericCoefficient = n
+	return c
+}
+func (c *ExponentedTerm) SetVariableCoefficient(v rune) Term {
+	c.v.VariableCoefficient = v
+	return c
+}
+
+func (c *ExponentedTerm) GetTerm() TermP {
+	return c.v
 }
